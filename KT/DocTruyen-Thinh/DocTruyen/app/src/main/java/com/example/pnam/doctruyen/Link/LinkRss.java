09dff426f6d610329a0fdd8018cf3809840c403c
NhungNgo1997 <ntknhung97@gmail.com>
2019-04-13 10:05:59 +0700

    quang

---
+++ KT/DocTruyen-Thinh/DocTruyen/app/src/main/java/com/example/pnam/doctruyen/Link/LinkRss.java
@@ -0,0 +1,7 @@
+package com.example.pnam.doctruyen.Link;
+
+public class LinkRss {
+    public final static String VNEXPRESS_RSS_CUOI = "https://vnexpress.net/rss/tin-moi-nhat.rss";
+    public final static String GET_TRUYEN_THIEU_NHI = "https://vnexpress.net/rss/du-lich.rss";
+    public final static String GET_TRUYEN_CO_TICH_VIET_NAM = "https://vnexpress.net/rss/giai-tri.rss";
+}
